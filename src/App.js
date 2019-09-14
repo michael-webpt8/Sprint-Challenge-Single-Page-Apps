e331f62bfba61d92179ab80b09869c049ad5d381
Michael Williams <manicdesigner@gmail.com>
2019-09-14 12:29:26 -0400

    format app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import TabNav from "./components/TabNav.js";
-import Header from "./components/Header.js";
+import React from 'react';
+import TabNav from './components/TabNav.js';
+import Header from './components/Header.js';
 
+import CharacterList from './components/CharacterList';
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
       {/* <TabNav /> */}
     </main>
   );
