aba241c10a56df8b8d936e3a4cda42817c9eb9e8
Michael Williams <manicdesigner@gmail.com>
2019-09-14 14:02:12 -0400

    build out LocationList

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {Spinner} from 'react-bootstrap';
 
-export default function LocationsList() {}
+import LocationCard from './LocationCard';
+
+export default function LocationsList() {
+  const [locations, setLocations] = useState();
+
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/location/?page=1')
+      .then(res => setLocations(res.data.results))
+      .catch(err => console.log(err));
+  }, []);
+  console.log('l', locations);
+  if (!locations)
+    return (
+      <Spinner animation='border' role='status'>
+        <span className='sr-only'>Loading...</span>
+      </Spinner>
+    );
+  return (
+    <div>
+      <h1>Location Lis</h1>
+      <div>
+        {locations.map(location => {
+          return <LocationCard location={location} />;
+        })}
+      </div>
+    </div>
+  );
+}
