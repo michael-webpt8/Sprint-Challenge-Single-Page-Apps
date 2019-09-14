cc792f8d97a5673bd17c557b44219a4e6f5dc8d9
Michael Williams <manicdesigner@gmail.com>
2019-09-14 12:57:52 -0400

    characterlist is redeuced from doing card and list

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
+import {Card} from 'react-bootstrap';
+
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [characters, setCharacters] = useState({});
+  const [characters, setCharacters] = useState();
   useEffect(() => {
     axios
-      .get('https://rickandmortyapi.com/api/character/')
+      .get(`https://rickandmortyapi.com/api/character/?page=1`)
       // res.data.results
       .then(res => setCharacters(res.data.results))
       .catch(err => console.log(err));
@@ -14,9 +16,23 @@ export default function CharacterList() {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
   console.log('c', characters);
+  if (!characters) return <h2>Loading...</h2>;
+
   return (
     <section className='character-list grid-view'>
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <Card key={character.created}>
+          <Card.Img src={character.image} alt={character.name} />
+          <Card.Body>
+            <Card.Title>{character.name}</Card.Title>
+            <Card.Subtitle>
+              {character.species} {character.status}
+            </Card.Subtitle>
+            <Card.Text>Location: {character.location.name}</Card.Text>
+            <Card.Text>Origin: {character.origin.name}</Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </section>
   );
 }
