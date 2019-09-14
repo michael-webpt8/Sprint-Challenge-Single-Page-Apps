ae76eb4e9a6b1b06fed4d08cddb938d757496f42
Michael Williams <manicdesigner@gmail.com>
2019-09-14 13:00:57 -0400

    modify the bootstrap placement and import CharacterCard in

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-
-import {Card} from 'react-bootstrap';
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
@@ -21,17 +20,7 @@ export default function CharacterList() {
   return (
     <section className='character-list grid-view'>
       {characters.map(character => (
-        <Card key={character.created}>
-          <Card.Img src={character.image} alt={character.name} />
-          <Card.Body>
-            <Card.Title>{character.name}</Card.Title>
-            <Card.Subtitle>
-              {character.species} {character.status}
-            </Card.Subtitle>
-            <Card.Text>Location: {character.location.name}</Card.Text>
-            <Card.Text>Origin: {character.origin.name}</Card.Text>
-          </Card.Body>
-        </Card>
+        <CharacterCard character={character} />
       ))}
     </section>
   );
