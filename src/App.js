c4230f622e0aaf4d79a9ed244d5523b497e68568
Michael Williams <manicdesigner@gmail.com>
2019-09-14 14:28:07 -0400

    add Route to /, characters, locations, episodes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import TabNav from './components/TabNav.js';
 import Header from './components/Header.js';
+import {Route} from 'react-router-dom';
 
 import CharacterList from './components/CharacterList';
 import LocationsList from './components/LocationsList';
 import EpisodeList from './components/EpisodeList';
+import WelcomePage from './components/WelcomePage';
 
 export default function App() {
   return (
@@ -14,6 +16,11 @@ export default function App() {
       <TabNav />
       {/* <EpisodeList /> */}
       {/* <CharacterList /> */}
+
+      <Route exact path='/' component={WelcomePage} />
+      <Route exact path='/characters' component={CharacterList} />
+      <Route exact path='/locations' component={LocationsList} />
+      <Route exact path='/episodes' component={EpisodeList} />
     </main>
   );
 }
