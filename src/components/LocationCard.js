c1a575900d7741a4c2ebae3c4708450f2fa4e515
Michael Williams <manicdesigner@gmail.com>
2019-09-14 14:02:00 -0400

    Create LocationCard

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React from 'react';
+import {Card, Spinner} from 'react-bootstrap';
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard(
+  props,
+  {
+    id,
+    name,
+    type,
+    dimension
+    // residents
+  }
+) {
+  console.log('LC', props.location);
+  if (!props.props) return <h1>loading..</h1>;
+
+  return (
+    <Card key={id}>
+      <Card.Header>{name}</Card.Header>
+      <Card.Body>
+        <Card.Text>
+          {type} {dimension}
+        </Card.Text>
+      </Card.Body>
+      {/* <Card.Footer> Residents {residents.length}</Card.Footer> */}
+      {/** !!! could not get want to know why !!! */}
+    </Card>
+  );
 }
