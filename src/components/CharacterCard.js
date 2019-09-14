1402f8e9eb1a85bb3642e6dbf792a922970f2671
Michael Williams <manicdesigner@gmail.com>
2019-09-14 12:57:30 -0400

    charactercard is create from characterlist

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React from 'react';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+import {Card} from 'react-bootstrap';
+
+export default function CharacterCard(props) {
+  const {characters} = props;
+  return (
+    <Card key={character.created}>
+      <Card.Img src={character.image} alt={character.name} />
+      <Card.Body>
+        <Card.Title>{character.name}</Card.Title>
+        <Card.Subtitle>
+          {character.species} {character.status}
+        </Card.Subtitle>
+        <Card.Text>Location: {character.location.name}</Card.Text>
+        <Card.Text>Origin: {character.origin.name}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
