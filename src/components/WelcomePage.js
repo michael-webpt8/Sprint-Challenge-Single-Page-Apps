abbd443c34fdbb5b06ea91055988232d95aa24de
Michael Williams <manicdesigner@gmail.com>
2019-09-15 17:09:40 -0400

    mod to back to image to use classes given fixed other styled unneeded  borought back semantiuui

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import Header from './Header';
-
+import { Card } from 'reactstrap';
 import styled from 'styled-components';
 
 export default function WelcomePage() {
-  return (
-    <section className='welcome-page'>
-      <WelcomeWrapper>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className='main-img ui centered medium circular image'
-          src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-          alt='rick'
-        />
-      </WelcomeWrapper>
-    </section>
-  );
+	return (
+		<section className="welcome-page">
+			<header>
+				<h1>Welcome to the ultimate fan site!</h1>
+				<img
+					className="main-img ui centered medium circular image"
+					src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+					alt="rick"
+				/>
+			</header>
+		</section>
+	);
 }
-
-const WelcomeWrapper = styled.header`
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
-`;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
 );
