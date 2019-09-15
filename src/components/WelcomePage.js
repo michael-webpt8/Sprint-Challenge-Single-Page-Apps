eee701e931ecc4a9242968cdd0a90fd5f7cdcdd1
Michael Williams <manicdesigner@gmail.com>
2019-09-15 18:38:39 -0400

    welcome page links to characters

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Header from './Header';
+import { Link } from 'react-router-dom';
 import { Card } from 'reactstrap';
 import styled from 'styled-components';
 
 export default function WelcomePage() {
-	return (
-		<section className="welcome-page">
-			<header>
-				<h1>Welcome to the ultimate fan site!</h1>
-				<img
-					className="main-img ui centered medium circular image"
-					src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-					alt="rick"
-				/>
-			</header>
-		</section>
-	);
+  return (
+    <section className='welcome-page'>
+      <header>
+        <h1>Welcome to the ultimate fan site!</h1>
+        <Link to='/characters'>
+          <img
+            className='main-img ui centered medium circular image'
+            src='https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+            alt='rick'
+          />
+        </Link>
+      </header>
+    </section>
+  );
 }
